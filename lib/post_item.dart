4b0daf84e2e1288c934bb65067f4f31fcada98cd
Tobias Emrich <tobiemrich@gmail.com>
2022-03-24 16:56:40 +0100

    open link in inapp browser

--- lib/post_item.dart
+++ lib/post_item.dart
@@ -1,59 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:medium_tags/post.dart';
 import 'package:medium_tags/scraper.dart';
 
-class PostItem extends StatelessWidget {
-  const PostItem({Key? key, required Post post})
+class PostItem extends StatefulWidget {
+  PostItem({Key? key, required Post post})
       : _post = post,
         super(key: key);
 
   final Post _post;
+  final ChromeSafariBrowser _browser = ChromeSafariBrowser();
 
+  @override
+  State<PostItem> createState() => _PostItemState();
+}
+
+class _PostItemState extends State<PostItem> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(child: Text(_post.author)),
-                Container(
-                  decoration: BoxDecoration(
-                    color: ColorX.getColorFromDate(_post.publishedDate),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Text(
-                      _post.publishedDate.toMediumDate(),
-                      style: TextStyle(
-                        color: ColorX.getColorFromDate(_post.publishedDate)
-                            .getTextColor(),
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.bold,
+      child: InkWell(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(child: Text(widget._post.author)),
+                  Container(
+                    decoration: BoxDecoration(
+                      color:
+                          ColorX.getColorFromDate(widget._post.publishedDate),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(
+                        widget._post.publishedDate.toMediumDate(),
+                        style: TextStyle(
+                          color: ColorX.getColorFromDate(
+                                  widget._post.publishedDate)
+                              .getTextColor(),
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: Text(
-                _post.title,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 14.0,
-                ),
+                ],
               ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(
+                  widget._post.title,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14.0,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
+        onTap: () async {
+          await widget._browser.open(url: Uri.parse(widget._post.link));
+        },
       ),
     );
   }
